c2aec94303435323b55eabbb51582177a8a7b74e
Gabriel Perez <gabrielmpp@protonmail.com>
2019-12-01 14:57:05 +0000

    included xarray mode

--- upscale_api/api.py
+++ upscale_api/api.py
@@ -1,7 +1,9 @@
-from dictionary import load_dictionary
+from upscale_api.dictionary import load_dictionary
 import os
 import fnmatch
-import iris
+import pandas as pd
+import xarray as xr
+import numpy as np
 import glob
 
 def display_variables():
@@ -14,6 +16,8 @@ def display_variables():
         variables.append(variable)
     print(variables)
     return variables
+
+
 def display_resolutions_and_simulations():
     print('''Available resolutions are:\n
            N512: xgxqe  xgxqf  xgxqg  xgxqh  xgxqi  xgxqj\n
@@ -23,13 +27,12 @@ def display_resolutions_and_simulations():
 
 
 class upscale():
-    def __init__(self,resolution,simulation,variable,time_scale,climate,region=None,year=None):
+    def __init__(self, resolution, simulation, variable, time_scale, climate, year=None):
         self.base_path = base_path = '/group_workspaces/jasmin4/upscale/GA3/{climate}_climate/'.format(climate=climate)
         self.resolution = resolution
         self.variable = variable
         self.time_scale = time_scale
         self.climate = climate
-        self.region = region
         self.year = year
         self.simulation = simulation
 
@@ -48,7 +51,7 @@ class upscale():
         nitems = len(lKey)
         for i in range(0,nitems):
             path = os.path.join(self.base_path,self.resolution,self.time_scale,lKey[i],self.simulation)
-            print 'Trying path ' + path
+            print('Trying path ' + path)
             if os.path.isdir(path):
                 self.path=path
                 return path
@@ -56,39 +59,112 @@ class upscale():
                 break
         raise Exception('Could not find dir')
 
+    def createDomains(region, reverseLat=False):
+        if region == "SACZ_big":
+            domain = dict(latitude=[-40, 5], longitude=[-70, -20])
+        elif region == "SACZ":
+            domain = dict(latitude=[-40, -5], longitude=[-62, -20])
+        elif region == "SACZ_small":
+            domain = dict(latitude=[-30, -20], longitude=[-50, -35])
+        elif region == "AITCZ":
+            domain = dict(latitude=[-5, 15], longitude=[-45, -1])
+        elif region == "NEBR":
+            # domain = dict(latitude=[-15, 5], longitude=[-45, -15])
+            domain = dict(latitude=[-10, 5], longitude=[-55, -40])
+        elif region is None:
+            domain = dict(latitude=[None, None], longitude=[None, None])
+        else:
+            raise ValueError(f'Region {region} not supported')
 
+        if reverseLat:
+            domain = dict(latitude=slice(domain['latitude'][1], domain['latitude'][0]),
+                          longitude=slice(domain['longitude'][0], domain['longitude'][1]))
 
-    def _create_bounding_box(self,region = 'South America'):
+        else:
+            domain = dict(latitude=slice(domain['latitude'][0], domain['latitude'][1]),
+                          longitude=slice(domain['longitude'][0], domain['longitude'][1]))
 
-      if region == 'South America':
-          limits = [270, 330, -53, 15]
-      else:
-          limits = [0, 360, -90, 90]
+        return domain
 
-      bbox = iris.Constraint(longitude=lambda cell: limits[0] <= cell <= limits[1],
-                                latitude=lambda cell: limits[2] <= cell <= limits[3])
+    def read_nc_files(self, filepaths, region=None,
+                      transformLon=False, lonName="longitude", reverseLat=False,
+                      time_slice_for_each_year=slice(None, None), season=None, lcstimelen=None, set_date=False,
+                      binary_mask=None, maskdims={'latitude': 'lat', 'longitude': 'lon'}):
+        """
 
-      return bbox
+        :param binary_mask:
+        :param transformLon:
+        :param lonName:
+        :param region:
+        :param basepath:
+        :param filename:
+        :param year_range:
+        :return:
+        """
+        print("*---- Starting reading data ----*")
+        file_list = []
 
-    def _readConc_cubes(self,files_name,region):
+        def transform(x, binary_mask):
+            if transformLon:
+                x.coords[lonName].values = \
+                    (x.coords[lonName].values + 180) % 360 - 180
+            if isinstance(region, dict):
+                x = x.sel(region)
+            if not isinstance(season, type(None)):
 
-        bbox = self._create_bounding_box(region)
-        cubes = iris.load(files_name, bbox) # TODO: find a better solution, now im just skipping smaller cubes
-        #cube = cubes.concatenate_cube()
-        cube = cubes.concatenate()
-        print '--- Selecting the first cube of the list below ---'
-        print(cube)
-        return cube[0] #returning just the first cube that i expect to be the most complete
+                if season == 'DJF':
+                    season_idxs = np.array([pd.to_datetime(t).month in [1, 2, 12] for t in x.time.values])
+                elif season == 'JJA':
+                    season_idxs = np.array([pd.to_datetime(t).month in [5, 6, 7] for t in x.time.values])
+                else:
+                    raise ValueError(f"Season {season} not supported")
+                x = x.sel(time=x.time[season_idxs])
+            if isinstance(binary_mask, xr.DataArray):
+                binary_mask = binary_mask.where(binary_mask == 1, drop=True)
+                x = x.sel(latitude=binary_mask[maskdims['latitude']].values,
+                          longitude=binary_mask[maskdims['longitude']].values, method='nearest')
+                binary_mask = binary_mask.rename({maskdims['longitude']: 'longitude', maskdims['latitude']: 'latitude'})
+                x = x.where(binary_mask == 1, drop=True)
 
-    def read_cubes(self):
+            return x
 
-        print 'Reading cubes  for variable {} and resolution {}'.format(self.variable,self.resolution)
+        for i, file in enumerate(filepaths):
+            print(f'Reading file {file}')
+            filename_formatted = filepaths[i]
+            print(filename_formatted)
+            array = None
+            fs = (xr.open_dataarray, xr.open_dataset)
+            for f in fs:
+                try:
+                    array = f(filename_formatted)
+                except ValueError:
+                    print('Could not open file using {}'.format(f.__name__))
+                else:
+                    break
+
+            if isinstance(array, (xr.DataArray, xr.Dataset)):
+                file_list.append(transform(array, binary_mask))
+            else:
+                print(f'Unable to read {file}.')
+        full_array = xr.concat(file_list, dim='time')
+        print('*---- Finished reading data ----*')
+        return full_array
+
+    def read_cubes(self, lat_min=None, lon_min=None, lat_max=None, lon_max=None, region=None):
+        if isinstance(region, type(None)):
+            region = dict(
+                latitude=slice(lat_min, lat_max),
+                longitude=slice(lon_min, lon_max)
+            )
+        else:
+            region = self.createDomains(region)
+
+        print('Reading cubes  for variable {} and resolution {}'.format(self.variable,self.resolution))
         print('=' * 20)
         var_path = self._create_path()
         print(var_path)
         files_list = [File for File in os.listdir(var_path) if fnmatch.fnmatch(File,'*{year}*.nc'.format(year=self.year)) and not fnmatch.fnmatch(File,'*'+self.variable+'*')] # list files and ignores symlinks
         print(files_list)
-        files_name = [os.path.join(var_path,files_list[i]) for i in range(len(files_list))]
-        cube = self._readConc_cubes(files_name,self.region)
-        cube.var_name = self.variable
+        files_name = [os.path.join(var_path, files_list[i]) for i in range(len(files_list))]
+        cube = self.read_nc_files(self, files_name, region)
         return cube
