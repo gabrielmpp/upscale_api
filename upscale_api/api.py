24318032978f6522fe011d56c541321767ce02b9
Gabriel Perez <gabrielmpp@protonmail.com>
2019-12-01 18:31:33 +0000

    included xarray mode

--- upscale_api/api.py
+++ upscale_api/api.py
@@ -111,12 +111,12 @@ class upscale():
             if isinstance(region, dict):
                 if dif_lon > 0:
                     x = x.roll(longitude=-dif_lon, roll_coords=True)
+                    x = x.sortby('longitude')
                     x = x.roll(longitude=-dif_lon, roll_coords=False)
                     print(x)
                     print(region)
                     x = x.sel(region)
                     x = x.roll(longitude=dif_lon, roll_coords=False)
-                    x = x.roll(longitude=dif_lon, roll_coords=True)
                 else:
                     x = x.sel(region)
             if not isinstance(season, type(None)):
